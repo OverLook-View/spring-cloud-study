3488c1ac86ffa9c51834d960601d64299d802dc8
OverLook-View <492055737@qq.com>
2018-03-08 09:51:16 +0800

    springcloud config server & client

---
+++ springcloud-config-client/src/main/java/com/example/sy/springcloud/config/springcloudconfigclient/SpringcloudConfigClientApplication.java
@@ -0,0 +1,24 @@
+package com.example.sy.springcloud.config.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringcloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	private String foo;
+
+	@RequestMapping("hi")
+	public String hi(){
+		return foo;
+	}
+}
