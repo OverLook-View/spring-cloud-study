6200e8c4ee9edb4ef7dc39b733d28831d1a66d23
OverLook-View <492055737@qq.com>
2018-03-12 15:43:57 +0800

    spring cloud Sleuth zipkin

---
+++ srpingcloud-zipkin-service/src/test/java/com/example/sy/springcloud/zipkin/service/srpingcloudzipkinservice/SrpingcloudZipkinServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sy.springcloud.zipkin.service.srpingcloudzipkinservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrpingcloudZipkinServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
