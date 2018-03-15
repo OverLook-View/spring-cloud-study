5c33b7ce7d5a0bc95126392ddc1ed87708083b4d
OverLook-View <492055737@qq.com>
2018-03-15 15:21:52 +0800

    Hystrix Dashboard

--- springcloud-eureka-client/src/main/java/com/example/sy/springcloud/eurekaclient/springclouderurkaclient/springcloudeurekaclient/SpringcloudEurekaClientApplication.java
+++ springcloud-eureka-client/src/main/java/com/example/sy/springcloud/eurekaclient/springclouderurkaclient/springcloudeurekaclient/SpringcloudEurekaClientApplication.java
@@ -1,9 +1,12 @@
 package com.example.sy.springcloud.eurekaclient.springclouderurkaclient.springcloudeurekaclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class SpringcloudEurekaClientApplication {
 
-	@Value("${server.port}")
-	private String port;
-	public static void main(String[] args) {
-		SpringApplication.run(SpringcloudEurekaClientApplication.class, args);
-	}
+    @Value("${server.port}")
+    private String port;
 
-	@RequestMapping("/hello")
-	public String hello(@RequestParam String name){
-		return "hello,this is "+name+",port: "+port;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaClientApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hello(@RequestParam String name) {
+        return "hello,this is " + name + ",port: " + port;
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ".sorry,error!";
+    }
 }
